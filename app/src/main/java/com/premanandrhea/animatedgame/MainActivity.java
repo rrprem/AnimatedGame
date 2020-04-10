cc0f1646d203c08e21cff2667afb645f6c6a929d
rrprem <rheapremanand@gmail.com>
2020-04-10 17:51:51 -0400

    Initial commit

---
+++ app/src/main/java/com/premanandrhea/animatedgame/MainActivity.java
@@ -0,0 +1,14 @@
+package com.premanandrhea.animatedgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
